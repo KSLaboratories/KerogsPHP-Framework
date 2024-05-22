9aa7dfd51f12aad33f686454acb980d44afd1913
kerogs <luckywies@gmail.com>
2024-05-23 00:43:30 +0200

    added the link

--- public/index.php
+++ public/index.php
@@ -21,8 +21,8 @@ require_once('../config.php');
         <img src="src/img/logo.png" alt="Dragon Knowledge Logo" class="logo">
         <h1>Dragon Knowledge - <?=$dkf_version?></h1>
         <div class="buttons">
-            <a href="https://github.com/votre-repo" class="btn">Github</a>
-            <a href="https://votre-documentation" class="btn">Documentation</a>
+            <a href="https://github.com/KSInfinite/Dragon-Knowledge-Framework" class="btn">Github</a>
+            <!-- <a href="" class="btn">Documentation</a> -->
         </div>
     </div>
 
