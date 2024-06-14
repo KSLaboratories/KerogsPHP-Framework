6229ff767bbeeeccaf05ed6ba8a34668f058259c
kerogs <luckywies@gmail.com>
2024-06-14 22:34:22 +0200

    remove download PWA button

--- public/index.php
+++ public/index.php
@@ -19,9 +19,6 @@ require_once('../config.php');
 
     <body>
 
-        <button onclick="installPWA()">Télécharger l'application</button>
-
-
         <div id="page">
             <div id="header">
                 <div class="logo">
@@ -115,36 +112,5 @@ require_once('../config.php');
     </body>
 
 </body>
-<script>
-    let deferredPrompt;
-
-    window.addEventListener('beforeinstallprompt', (e) => {
-        // Prevent Chrome 67 and earlier from automatically showing the prompt
-        e.preventDefault();
-        // Stash the event so it can be triggered later.
-        deferredPrompt = e;
-        // Update UI to notify the user they can add to home screen
-        showInstallButton();
-    });
-
-    function showInstallButton() {
-        const installButton = document.querySelector('button');
-        installButton.style.display = 'block';
-    }
-
-    function installPWA() {
-        // Show the prompt
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the A2HS prompt');
-            } else {
-                console.log('User dismissed the A2HS prompt');
-            }
-            deferredPrompt = null;
-        });
-    }
-</script>
 
 </html>
