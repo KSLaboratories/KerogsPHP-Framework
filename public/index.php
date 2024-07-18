7c5319f68ffcdfb5006da84a382433e34239e119
kerogs <luckywies@gmail.com>
2024-07-18 15:03:08 +0200

    move robots.txt, change welcome interface, and more..
    add HTML dir in /public/src/html/
    add PHP dir in /public/src/php/
    move robots.txt from /robots.txt -> /public/robots.txt
    Welcome message
    Welcome interface completely modified and now used as a tutorial
    Now supports dependabot for composer and NPM
    update framwork version from 2.1.1 -> 2.2.1

--- public/index.php
+++ public/index.php
@@ -11,102 +11,121 @@ require_once('../config.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php require_once '../inc/head.php' ?>
-    <title><?= $kpf_config["seo"]["title_short"] ?> | Good Job</title>
+    <title><?= $kpf_config["seo"]["title_short"] ?></title>
     <link rel="stylesheet" href="src/css/style.css">
+
+    <!-- src -->
+    <link rel="stylesheet" href="./node_modules/boxicons/css/boxicons.min.css">
 </head>
 
 <body>
 
-    <div id="page">
-        <div id="header">
-            <div class="logo">
-                <img src="/src/img/logo.png" alt="Logo">
+    <nav>
+        <div class="logo">
+            <img src="src/img/logo.png" alt="logo">
+        </div>
+        <hr>
+        <ul>
+            <li class="active"><i class='bx bx-book-alt'></i></li>
+            <li><i class='bx bx-donate-heart'></i></li>
+        </ul>
+    </nav>
+
+    <main>
+        <header>
+            <ul>
+                <li>v<?= $kpf_config["framework"]["framework_version"] ?> <i class='bx bxl-github'></i></li>
+            </ul>
+        </header>
+
+        <div class="container">
+            <div class="container__title">
+                <h1>Welcome to <span><?= $kpf_config['framework']['title'] ?></span> 👋</h1>
+                <p>Follow the steps below to get you started.</p>
             </div>
-            <div class="title">
-                <h1><?= $kpf_config["seo"]["title"] ?></h1>
-                <p>Version <?= $kpf_config["framework"]["framework_version"] ?> <span class="url">- <a href="<?= $kpf_urlHTTP . $kpf_urlHOST ?>"><?= $kpf_urlHTTP . $kpf_urlHOST ?></a></span></p>
+
+            <div class="container__card">
+                <div class="subtitle">
+                    <h2>Lets get started !</h2>
+                    <h2><span id="clickedCard">?</span>/<span id="totalCard">?</span></h2>
+                </div>
+                <div class="bar">
+                    <div id="statusBar"></div>
+                </div>
+
+                <!-- cards -->
+                <div class="objectif">
+
+                    <div class="objectif__card" id="congratulations">
+                        <div class="icon">
+                            <i class='bx bx-happy-beaming'></i>
+                        </div>
+                        <div class="name">
+                            <h3>Congratulations</h3>
+                            <p>Congratulations, KerogsPHP framework has been installed.</p>
+                        </div>
+                    </div>
+
+                    <div class="objectif__card" id="structure-file">
+                        <div class="icon">
+                            <i class='bx bx-sitemap'></i>
+                        </div>
+                        <div class="name">
+                            <h3>structure of files</h3>
+                            <p>Discover the framework's file tree.</p>
+                        </div>
+                    </div>
+
+                    <div class="objectif__card" id="framework-basics">
+                        <div class="icon">
+                            <i class='bx bx-package'></i>
+                        </div>
+                        <div class="name">
+                            <h3>Framework basics</h3>
+                            <p>Learn the basics of the framework and how to use it.</p>
+                        </div>
+                    </div>
+
+                    <div class="objectif__card" id="npm-and-composer">
+                        <div class="icon">
+                        <i class='bx bxs-component' ></i>
+                        </div>
+                        <div class="name">
+                            <h3>Using NPM and Composer.</h3>
+                            <p>Learn how to add new NPM packages and Composer dependencies with this short tutorial.</p>
+                        </div>
+                    </div>
+
+                    <div class="objectif__card" id="sass">
+                        <div class="icon">
+                            <i class='bx bxl-sass'></i>
+                        </div>
+                        <div class="name">
+                            <h3>How to use SASS/SCSS</h3>
+                            <p>Learn how to use SASS and SCSS with the framework </p>
+                        </div>
+                    </div>
+
+                    <div class="objectif__card" id="typescript">
+                        <div class="icon">
+                            <i class='bx bxl-typescript'></i>
+                        </div>
+                        <div class="name">
+                            <h3>How to use TypeScript</h3>
+                            <p>Learn how to use TypeScript with the framework.</p>
+                        </div>
+                    </div>
+
+                    <!-- cards end -->
+
+                </div>
+
             </div>
         </div>
-        <div id="content">
-            <h2>Well done, successful installation!</h2>
-            <p>
-                Here is the default homepage used to test the correct operation of the server after installing the KerogsPHP Framework. If you can read this page, it means the framework installed on this site is working correctly. You should <b>replace this file</b> (located at <tt>/public/index.php</tt>) before continuing to use your site.
-            </p>
-            <p>
-                If you are a regular user of this website and are unsure about its current status, it likely means the site is temporarily unavailable due to maintenance. If the issue persists, please contact the site administrator for further assistance.
-            </p>
-            <h3>Configuration Overview</h3>
-            <p>
-                The default configuration of the KerogsPHP Framework differs from the upstream configuration, and is organized into several files optimized for interaction with the framework's tools. The configuration system is fully documented in the README file. For complete documentation, please refer to this file. Documentation for the web server itself can be accessed by referring to the relevant manuals if the necessary packages were installed on this server.
-            </p>
-            <p>
-                The configuration layout for a KerogsPHP Framework installation on Ubuntu systems is as follows:
-            </p>
-            <pre>
-📦KerogsPHP-Framework
- ┣ 📂.ksinf
- ┃ ┗ 📂.ignore
- ┣ 📂api
- ┣ 📂backend
- ┃ ┣ 📂data
- ┃ ┣ 📂func
- ┃ ┣ 📜core.php
- ┃ ┗ 📜core-labs.php
- ┣ 📂inc
- ┣ 📂public
- ┃ ┣ 📂error
- ┃ ┣ 📂src
- ┃ ┃ ┣ 📂css
- ┃ ┃ ┣ 📂img
- ┃ ┃ ┗ 📂scss
- ┃ ┣ 📜.htaccess
- ┃ ┣ 📜index.php
- ┃ ┗ 📜package.json
- ┣ 📂test
- ┃ ┣ 📂src
- ┃ ┗ 📜index.php
- ┣ 📜.gitignore
- ┣ 📜.htaccess
- ┣ 📜composer.json
- ┣ 📜config.php
- ┣ 📜config.yml
- ┣ 📜LICENSE
- ┣ 📜README.md
- ┗ 📜robots.txt
-</pre>
-            <h3>Installation</h3>
-            <p>
-                You'll need <tt>NodeJS</tt> and <tt>Composer</tt> installed.
-            </p>
-            <p>1. Clone the repository<b></b></p>
-            <p>
-            <pre>git clone https://github.com/KSInfinite/KerogsPHP-Framework.git</pre>
-            </p>
-            <p>2. <b>Go in the directory</b><br>
-            <pre>cd KerogsPHP-Framework</pre>
-            </p>
-            <p>3. <b>Install composer package</b><br>
-            <pre>composer install</pre>
-            </p>
-            <p>4. <b>Go in the public directory</b><br>
-            <pre>cd ./public</pre>
-            </p>
-            <p>5. <b>Install npm package</b><br>
-            <pre>npm install</pre>
-            </p>
-            <h3>Document Roots</h3>
-            <p>
-                The default folder for users is <tt>/public/</tt>. Everything else will be in the root folder.
-            </p>
-            <h3>Reporting Problems</h3>
-            <p>
-                Don't forget that this framework is still in the testing phase. <b>There may be security holes or bugs</b>. In this case, please make an <a href="https://github.com/KSInfinite/KerogsPHP-Framework/issues">issue</a>. Or help us. All this can be done from our <a href="https://github.com/KSInfinite/KerogsPHP-Framework">github page</a>.
-            </p>
-            <p>
-                If you encounter problems with other packages, please report them to their owners.
-            </p>
-        </div>
-    </div>
+
+        <?php require_once 'src/html/popup.htm' ?>
+
+    </main>
 
     <?php require_once '../inc/script.php' ?>
 </body>
